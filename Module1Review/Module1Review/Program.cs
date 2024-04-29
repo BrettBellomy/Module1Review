c75aa7541a03ef9b54fc3cbc8c809ab14918d21f
Brett Bellomy <brettbellomymusic@gmail.com>
2024-04-29 13:07:08 -0500

    added area of circle

--- Module1Review/Module1Review/Program.cs
+++ Module1Review/Module1Review/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
+        static double CircleArea(double radius)
+        {
+            return 3.14159 * radius * radius;
+        }
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter the radius of the circle");
+            double radius = (Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("The area of the circle is:");
+            CircleArea(radius);
         }
     }
 }
