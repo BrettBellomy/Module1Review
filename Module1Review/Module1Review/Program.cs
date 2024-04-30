4475ab663105713d268bf002cec6c906da2d3d44
Brett Bellomy <brettbellomymusic@gmail.com>
2024-04-30 09:20:57 -0500

    Added bonus challenge

--- Module1Review/Module1Review/Program.cs
+++ Module1Review/Module1Review/Program.cs
@@ -22,38 +22,48 @@ namespace Module1Review
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the radius of the circle");
-            double radius = (Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Enter the shape you would like to calculate the area of. Please enter the following:\n'Circle' for circle\n'Triangle' for triangle\n'Rectangle' for rectangle\n'Square' for square");
+            string shape = Console.ReadLine();
+            if (shape == "Circle")
+            {
+                Console.WriteLine("Please enter the radius of the circle");
+                double radius = (Convert.ToDouble(Console.ReadLine()));
 
-            Console.WriteLine("The area of the circle is:");
-            double areaOfCircle = CircleArea(radius);
-            Console.WriteLine(areaOfCircle);
+                Console.WriteLine("The area of the circle is:");
+                double areaOfCircle = CircleArea(radius);
+                Console.WriteLine(areaOfCircle);
+            }
+            if (shape == "Triangle")
+            {
+                Console.WriteLine("Please enter the length of the triangle");
+                double baseLength = (Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine("Please enter the height of the triangle");
+                double height = (Convert.ToDouble(Console.ReadLine()));
 
-            Console.WriteLine("Please enter the length of the triangle");
-            double baseLength = (Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Please enter the height of the triangle");
-            double height = (Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine("The area of the triangle is:");
+                double areaOfTriangle = TriangleArea(baseLength, height);
+                Console.WriteLine(areaOfTriangle);
+            }
+            if (shape == "Rectangle")
+            {
+                Console.WriteLine("Please enter the length of the rectangle");
+                double length = (Convert.ToDouble(Console.ReadLine()));
+                Console.WriteLine("Please enter the width of the rectangle");
+                double width = (Convert.ToDouble(Console.ReadLine()));
 
-            Console.WriteLine("The area of the triangle is:");
-            double areaOfTriangle = TriangleArea(radius, height);
-            Console.WriteLine(areaOfTriangle);
+                Console.WriteLine("The are of the rectangle is:");
+                double areaOfRectangle = RectangleArea(length, width);
+                Console.WriteLine(areaOfRectangle);
+            }
+            if (shape == "Square")
+            {
+                Console.WriteLine("Please enter the side length of  the square");
+                double side = (Convert.ToDouble(Console.ReadLine()));
 
-            Console.WriteLine("Please enter the length of the rectangle");
-            double length = (Convert.ToDouble(Console.ReadLine()));
-            Console.WriteLine("Please enter the width of the rectangle");
-            double width = (Convert.ToDouble(Console.ReadLine()));
-
-            Console.WriteLine("The are of the rectangle is:");
-            double areaOfRectangle = RectangleArea(length, width);
-            Console.WriteLine(areaOfRectangle);
-
-            Console.WriteLine("Please enter the side length of  the square");
-            double side = (Convert.ToDouble(Console.ReadLine()));
-
-            Console.WriteLine("The are of the square is:");
-            double areaOfSquare = SquareAre(side);
-            Console.WriteLine(areaOfSquare);
-
+                Console.WriteLine("The are of the square is:");
+                double areaOfSquare = SquareAre(side);
+                Console.WriteLine(areaOfSquare);
+            }
         }
     }
 }
--- Module1Review/Module1Review/obj/Debug/net8.0/Module1Review.AssemblyInfo.cs
+++ Module1Review/Module1Review/obj/Debug/net8.0/Module1Review.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47003c9bdf687b443fd321e28f4c506755eb0f20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d2d776d6727fe01957331d28bc9d8b433600a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Module1Review")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
